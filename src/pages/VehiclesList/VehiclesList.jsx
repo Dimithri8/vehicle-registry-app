c3e6cf32c7a1a82801806873bd8c1418939652f1
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-09 23:11:16 +0530

    Lifted all child component logic to vehicle list page

--- src/pages/VehiclesList/VehiclesList.jsx
+++ src/pages/VehiclesList/VehiclesList.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
 import Header from "../../components/Header/Header";
 import VehiclesTable from "../../components/VehiclesTable/VehiclesTable";
+import VehicleForm from "../../components/VehicleForm/VehicleForm";
 export default function VehiclesList() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [newVehicle, setNewVehicle] = useState({
+    licensePlate: "",
+    ownerName: "",
+    make: "",
+    model: "",
+    status: "Active",
+  });
+  const [allVehicles, setAllVehicles] = useState([]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNewVehicle((prevValue) => ({ ...prevValue, [name]: value }));
+  }
+  function addNewVehicle(e) {
+    e.preventDefault();
+    setAllVehicles((prevValue) => [...prevValue, newVehicle]);
+    setNewVehicle("");
+    setIsOpen(false);
+  }
+  function handleOpenForm() {
+    setIsOpen(true);
+  }
+  function handleCloseForm() {
+    setIsOpen(false);
+  }
+
   return (
     <>
-      <Header />
-      <VehiclesTable />
+      <Header handleOpenForm={handleOpenForm} />
+      <VehicleForm
+        isOpen={isOpen}
+        handleCloseForm={handleCloseForm}
+        handleChange={handleChange}
+        newVehicle={newVehicle}
+        handleSubmit={addNewVehicle}
+      />
+      <VehiclesTable vehicles={allVehicles} />
     </>
   );
 }
