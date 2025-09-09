c85cb410a17e6108f5029e4a70474291aa879185
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-09 19:59:46 +0530

    Added header and vehicles table components to the page

---
+++ src/pages/VehiclesList/VehiclesList.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header/Header";
+import VehiclesTable from "../../components/VehiclesTable/VehiclesTable";
+export default function VehiclesList() {
+  return (
+    <>
+      <Header />
+      <VehiclesTable />
+    </>
+  );
+}
